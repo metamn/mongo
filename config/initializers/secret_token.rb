52e06434cd570f72e9a98e7bf74302771d5cc533
rubycs <rubycs@clair.ro>
2010-11-29 20:33:45 +0200

    Mongoid added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongo::Application.config.secret_token = '73787a5267dae3a9625921972e9ac13a5bd8525c21d25b1e9b72d01d8d50878a5a189db9f77ce58c4f5580e4b73e9b111f502314a36a6503de0ae3b8d6998a4c'
